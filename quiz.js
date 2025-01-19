aba5c747b71a167a3a61818c78f1c3bb66f36a37
jean pierre <habipi05@yahoo.fr>
2025-01-19 22:50:48 +0200

    Completed the SUBMIT

---
+++ quiz.js
@@ -0,0 +1,18 @@
+//My js
+
+function checkAnswer() {
+    const correctAnswer ='4';
+    const userAnswer = document.querySelector('input[name="quiz"]:checked')?.value;
+
+        const feedbackElement = document.getElementById("feedback");
+
+        if (userAnswer === correctAnswer) {
+            feedbackElement.textContent = "Correct! Well done.";
+        } else {
+            feedbackElement.textContent = "That's incorect. Try again";
+        }
+}
+
+const submitButton = document.getElementById('submit-answer');
+submitButton.addEventListener("click", checkAnswer);
+
